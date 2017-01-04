fb1c0b480ec9b3d06a79b83419fa1f4b2ac969b4
Md.Shariful Islam <realdn@gmail.com>
2017-01-04 21:13:02 +0600

    Initial versions

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie();
+            movie.Id = 001;
+            movie.Name = "007 James Bond";                     
+            return View(movie);
+                                 
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);           
+        }
+        
+
+
+
+
+    }
+}
